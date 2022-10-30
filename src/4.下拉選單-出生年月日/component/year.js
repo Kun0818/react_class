a069ecca8e0540313b37b156a699d80e72f4bf5f
Kunda <love60066@gmail.com>
2022-10-31 00:32:12 +0800

    year層更新

--- src/4.下拉選單-出生年月日/component/year.js
+++ src/4.下拉選單-出生年月日/component/year.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-function Year({ setDataFromYear, dataFromYear }) {
+function Year({ setDataFromYear }) {
   const [year, setYear] = useState('')
 
   const yearList = []
